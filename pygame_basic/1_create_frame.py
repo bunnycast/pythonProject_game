40ff0e4bbacb8c5b56a30dc1fc7d5daeb3ca512d
Daniel Kim <berzzubunny@gmail.com>
2020-06-04 00:16:39 +0900

    feat: Pygame Project 1. Pygame Init 2. Making Game Frame

---
+++ pygame_basic/1_create_frame.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init() # 반드시 초기화
+
+# 화면 크기 설정
+screen_width = 480 
+screen_height = 640
+pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("My Game") # 게임이름
+
+# 이벤트 루프
+running = True # 게임이 돌고 있다
+while running:
+    for event in pygame.event.get():   # 이벤트 발생 체크
+        if event.type == pygame.QUIT: # X버튼 눌러 창을 끄는 이벤트 발생
+            running = False # 게임이 진행중이 아님
+
+# pygame 종료
+pygame.quit()
