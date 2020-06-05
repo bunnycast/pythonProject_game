dbe20a51962f23ba372add17269d566e46bdba15
Daniel Kim <berzzubunny@gmail.com>
2020-06-05 21:14:10 +0900

    feat: Making Background

--- pygame_basic/1_create_frame.py
+++ pygame_basic/1_create_frame.py
@@ -5,7 +5,7 @@ pygame.init() # 반드시 초기화
 # 화면 크기 설정
 screen_width = 480 
 screen_height = 640
-pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 # 화면 타이틀 설정
 pygame.display.set_caption("My Game") # 게임이름
